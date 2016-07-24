38f92a7d28477a398bdc48a53af79c56358fb3d3
向松涛 <280304286@163.com>
2016-07-24 10:00:52 +0800

    first submit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+// import App from "./App";
+import blogNav from "./blogNav";
+
+/* eslint-disable no-new */
+new Vue({
+    el: 'body',
+    data: {
+    },
+    methods: {
+    },
+    components: {blogNav}
+})
+
+
+
+
+
+
+// var MyComponent = Vue.extend({
+//     template:'<div>A custom component!</div>',
+//     replace:true
+// })
+
+// Vue.component('my-component', MyComponent)
+
+
+
+// new Vue({
+//     el: '#example',
+// })

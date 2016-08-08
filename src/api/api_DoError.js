f66525a658be9329b72bee953ef32612d6fcc6eb
向松涛 <280304286@163.com>
2016-08-08 17:18:00 +0800

    build

--- src/api/api_DoError.js
+++ src/api/api_DoError.js
@@ -8,17 +8,17 @@ import Vue from "vue";
 
 
 export const DoError = function (code) {
+    switch(parseInt(code)){
+
+    }
+
+
+    // code：1-成功；2~5-失败；8-数据库查找错误；9-非admin用户；10-token错误或超时（（Token 2h内有效）;
+
+
     return new Promise(function (resolve, reject) {
-        Vue.http.get(API.getArticleById.replace('id', articleId)).then((response) => {
-            // success callback
-            let result = response.data;
-            if (parseInt(result.code) === 1) {
-                resolve(result.data);
-            } else {
-                reject(parseInt(result.code));
-            }
-        }, () => {
-            reject(API.SYS_ERR);
-        });
+       if(code){
+
+       }
     });
 };

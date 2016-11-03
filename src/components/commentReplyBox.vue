028486837b1145efe6271114db709480dd5f20ab
向松涛 <280304286@163.com>
2016-11-03 14:36:05 +0800

    评论组件修改

--- src/components/commentReplyBox.vue
+++ src/components/commentReplyBox.vue
@@ -1,170 +1,184 @@
 <template>
-    <div class="commentBox" :class="{'active':!hasNickName}">
-        <!--问题盒子-->
-        <div class="commentBox__questionBox">
-            <input ng-focus="chain.selectId=''" class="commentBox__questionBox--input" type="text" placeholder="我要说几句" v-model="content">
+  <div class="commentBox" :class="{'active':!hasNickName}">
+    <!--问题盒子-->
+    <div class="commentBox__questionBox">
+      <input ng-focus="chain.selectId=''" class="commentBox__questionBox--input" type="text" placeholder="我要说几句"
+             v-model="content">
 
-            <button v-bind:disabled="!content" class="btn commentBox__questionBox--reply" @click="submit()">
-                <span>提交</span>
-            </button>
-        </div>
-        <!--个人信息-->
-        <div class="commentBox__info">
-            <div class="commentBox__info--input">
-                <label for="name">昵称:</label>
-                <input id="name" type="text" placeholder="请输入昵称!" v-model="name" required>
-            </div>
-            <div class="commentBox__info--input ">
-                <label for="mail">邮箱: </label>
-                <input type="email" id="mail" name="mail" placeholder="example@domain.com" v-model="email" required pattern="\w+@[a-z0-9]+\.[a-z]+" title="example@domain.com">
-            </div>
-        </div>
+      <button v-bind:disabled="!content" class="btn commentBox__questionBox--reply" @click="submit()">
+        <span>提交</span>
+      </button>
     </div>
+    <!--个人信息-->
+    <div class="commentBox__info">
+      <div class="commentBox__info--input">
+        <label for="name">昵称:</label>
+        <input id="name" type="text" placeholder="请输入昵称!" v-model="name" required>
+      </div>
+      <div class="commentBox__info--input ">
+        <label for="mail">邮箱: </label>
+        <input type="email" id="mail" name="mail" placeholder="example@domain.com" v-model="email" required
+               pattern="\w+@[a-z0-9]+\.[a-z]+" title="example@domain.com">
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
-    //base
-    @import "../theme/theme.scss";
+  //base
+  @import "../theme/theme.scss";
 
-    .commentBox {
+  .commentBox {
+    width: 100%;
+    box-sizing: border-box;
+    position: relative;
+    height: 50px;
+    transition: all ease 200ms;
+    z-index: 1;
+    margin: 0;
+    /*padding: 0 35px;*/
+    .commentBox__questionBox {
+      position: relative;
+      height: 50px;
+      z-index: 10;
+      .commentBox__questionBox--input {
         width: 100%;
-        box-sizing: border-box;
-        position: relative;
+        border-radius: 5px;
+        border: 1px solid $base-theme-color;
+        height: 100%;
+        padding-left: 20px;
+        padding-right: 78px;
+        &::-webkit-input-placeholder {
+          color: #a8a8a8;
+        }
+      }
+      .commentBox__questionBox--reply {
+
+        position: absolute;
+        right: 0;
+        top: 0;
         height: 50px;
+        box-sizing: border-box;
+        border-bottom-right-radius: 5px;
+        border-top-right-radius: 5px;
+
+        background: $base-theme-color;
+        color: #fff;
+        //height: 100%;
+        padding: 0 18px;
+        @include display-flex;
+        @include justify-content(center);
+        @include align-items(center);
+        font-size: 14px;
+
         transition: all ease 200ms;
-        z-index: 1;
-        margin: 0;
-        /*padding: 0 35px;*/
-        .commentBox__questionBox {
-            position: relative;
-            height: 50px;
-            z-index: 10;
-            .commentBox__questionBox--input {
-                width: 100%;
-                border-radius: 5px;
-                border: 1px solid $base-theme-color;
-                height: 100%;
-                padding-left: 20px;
-                padding-right: 78px;
-                &::-webkit-input-placeholder {
-                    color: #a8a8a8;
-                }
-            }
-            .commentBox__questionBox--reply {
-
-                position: absolute;
-                right: 0;
-                top: 0;
-                height: 50px;
-                box-sizing: border-box;
-                border-bottom-right-radius: 5px;
-                border-top-right-radius: 5px;
-
-                background: $base-theme-color;
-                color: #fff;
-                //height: 100%;
-                padding: 0 18px;
-                @include display-flex;
-                @include justify-content(center);
-                @include align-items(center);
-                font-size: 14px;
-
-                transition: all ease 200ms;
-                &:active,
-                &.activated {
-                    background: darken($base-theme-color, 10%);
-                }
-            }
+        &:active,
+        &.activated {
+          background: darken($base-theme-color, 10%);
         }
-        .commentBox__info {
-            @include display-flex;
-            @include justify-content(center);
-            @include align-items(center);
-            height: 50px;
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 1;
-            transition: all ease 200ms;
-            opacity: 0;
-            margin: 5px 0;
-            .commentBox__info--input {
-                /*flex: 1;*/
-                @include display-flex;
-                @include justify-content(center);
-                @include align-items(center);
-                margin: 0 10px;
-                color: #eee;
-
-                label {
-                    margin-right: 10px;
-
-                }
-                input {
-                    border-radius: 5px;
-                    border: 1px solid $base-theme-color;
-                    height: 32px;
-                    width: 220px;
-                    color: #464642;
-                    padding: 0 10px;
-                    font-size: 14px;
-                    &::-webkit-input-placeholder {
-                        color: #a8a8a8;
-                    }
-                }
-
-            }
-
-        }
-        &.active {
-            height: 100px;
-            .commentBox__info {
-                transform: translateY(50px);
-                opacity: 1;
-            }
-        }
+      }
     }
+    .commentBox__info {
+      @include display-flex;
+      @include justify-content(center);
+      @include align-items(center);
+      height: 50px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 1;
+      transition: all ease 200ms;
+      opacity: 0;
+      margin: 5px 0;
+      .commentBox__info--input {
+        /*flex: 1;*/
+        @include display-flex;
+        @include justify-content(center);
+        @include align-items(center);
+        margin: 0 10px;
+        color: #eee;
+
+        label {
+          margin-right: 10px;
+
+        }
+        input {
+          border-radius: 5px;
+          border: 1px solid $base-theme-color;
+          height: 32px;
+          width: 220px;
+          color: #464642;
+          padding: 0 10px;
+          font-size: 14px;
+          &::-webkit-input-placeholder {
+            color: #a8a8a8;
+          }
+        }
+
+      }
+
+    }
+    &.active {
+      height: 100px;
+      .commentBox__info {
+        transform: translateY(50px);
+        opacity: 1;
+      }
+    }
+  }
 
 </style>
-<script>
-    export default{
-        data(){
-            return {
-                content: '',
-                name: '',
-                email: '',
-            }
-        },
-        props: [
-            'hasNickName',
-            'articleId',
-            'preId',
-        ],
-        methods: {
-            submit: function () {
-                this.$dispatch('replyThisComment', {
-                    content: this.content,
-                    name: this.name,
-                    email: this.email,
-                    article_id: this.articleId,
-                    pre_id: this.preId,
-                })
-            }
-        },
-        ready: function () {
+<script type="text/javascript">
 
-        },
-        events:{
-            'Submitting':function () {
-                console.log("Submitting")
-            },
-            'SubmitSuccess':function () {
-                this.content='';
-                console.log("SubmitSuccess")
-            },
-            'SubmitFailure':function () {
-                console.log("SubmitFailure")
-            },
-        }
-    }
+  import Vue from 'vue';
+  var eventHub = new Vue()
+  console.log('eventHub')
+  console.log(eventHub)
+
+  export default{
+    data(){
+      return {
+        content: '',
+        name: '',
+        email: '',
+      }
+    },
+    props: [
+      'hasNickName',
+      'articleId',
+      'preId',
+      'eventHub',
+    ],
+    methods: {
+      submit: function () {
+        let _this = this;
+        // console.log('replyThisComment')
+        // console.log(_this.eventHub)
+        _this.eventHub.$emit('replyThisComment', {
+          content: this.content,
+          name: this.name,
+          email: this.email,
+          article_id: this.articleId,
+          pre_id: this.preId,
+        })
+      }
+    },
+    created:function () {
+      let _this = this;
+      _this.eventHub.$on('Submitting',function () {
+        console.log("Submitting")
+      });
+      _this.eventHub.$on('SubmitSuccess',function () {
+        _this.content = '';
+        console.log("SubmitSuccess")
+      });
+      _this.eventHub.$on('SubmitFailure',function () {
+        console.log("SubmitFailure")
+      });
+    },
+    mounted: function () {
+
+    },
+  }
+
 </script>

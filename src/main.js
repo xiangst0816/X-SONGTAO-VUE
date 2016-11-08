3375daaaa1c7e4c6312708bea55d2ea0415f6a68
向松涛 <280304286@163.com>
2016-11-08 10:21:48 +0800

    ua判断，并设为全局

--- src/main.js
+++ src/main.js
@@ -12,11 +12,17 @@ import "bootstrap/js/tooltip.js";
 import "bootstrap/js/modal.js";
 import "bootstrap/js/transition.js";
 import attachFastClick from "fastclick";
+import ua from './plugin/parseUA'
 
 /**
  * $router全局化，便于外部js调用
  * */
 window.$router = router;
+
+/**
+ * 浏览环境判断，保存为全局
+ * */
+window.ua = ua;
 
 /**
  * 触摸配置
@@ -40,6 +46,11 @@ if (process.env.NODE_ENV === 'production') {
   console.warn = function () {
   }
 }
+
+
+
+
+
 
 
 

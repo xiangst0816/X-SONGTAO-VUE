1191ba552f326dc58a2f7af4bd1e35da1afa56d8
向松涛 <280304286@163.com>
2017-06-25 19:06:24 +0800

    代码整理

--- src/main.js
+++ src/main.js
@@ -2,32 +2,32 @@
  * Created by xiangsongtao on 16/10/29.
  * Description: main.js
  * */
-'use strict';
-import Vue from 'vue';
-import App from './App';
-import router from './router.js'
-import "./theme/util.scss";
-import "bootstrap/scss/bootstrap.slim.scss";
-// import "bootstrap/js/tooltip.js";
-// import "bootstrap/js/modal.js";
-// import "bootstrap/js/transition.js";
-import attachFastClick from "fastclick";
+'use strict'
+import 'bootstrap/js/modal.js'
+import 'bootstrap/js/tooltip.js'
+import 'bootstrap/js/transition.js'
+import 'bootstrap/scss/bootstrap.slim.scss'
+import attachFastClick from 'fastclick'
+import Vue from 'vue'
+import App from './App'
 import ua from './plugin/parseUA'
+import router from './router.js'
+import './theme/util.scss'
 
 /**
  * $router全局化，便于外部js调用
  * */
-window.$router = router;
+window.$router = router
 
 /**
  * 浏览环境判断，保存为全局
  * */
-window.ua = ua;
+window.ua = ua
 
 /**
  * 触摸配置
  * */
-new attachFastClick(document.body);
+new attachFastClick(document.body)
 
 /**
  * 发布模式禁用console.log()
@@ -43,5 +43,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: {App},
+  components: {App}
 })
